713e483aac8d630718d3af253e25e75960f88318
Mickael Croquet <mickael.croquet@laposte.net>
2025-01-24 13:46:24 +0100

    feat(module4): solution module4

---
+++ MyApi/MyApi.WebApi/Services/IOpenMeteoService.cs
@@ -0,0 +1,9 @@
+﻿namespace MyApi.WebApi.Services
+{
+    public interface IOpenMeteoService
+    {
+        Task<MeteoServiceObject> GetMeteo(
+            float latitude,
+            float longitude);
+    }
+}
