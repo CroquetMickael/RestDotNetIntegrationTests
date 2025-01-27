ad296deaf84df172c65fdd134b4be5e98c8f04e6
Mickael Croquet <mickael.croquet@axa.fr>
2025-01-27 08:56:49 +0100

    feat(module2): Ajout de la solution du module 2

---
+++ MyApi/MyApi.WebApi.Tests/ValueRetriever/DateOnlyValueRetriever.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using TechTalk.SpecFlow.Assist.ValueRetrievers;
+
+public class DateOnlyValueRetriever : StructRetriever<DateOnly>
+{
+    /// <summary>
+    /// Gets or sets the DateTimeStyles to use when parsing the string value.
+    /// </summary>
+    /// <remarks>Defaults to DateTimeStyles.None.</remarks>
+    public static DateTimeStyles DateTimeStyles { get; set; } = DateTimeStyles.None;
+
+    protected override DateOnly GetNonEmptyValue(string value)
+    {
+        DateTime.TryParse(value, CultureInfo.CurrentCulture, DateTimeStyles, out DateTime dateTimeValue);
+        return DateOnly.FromDateTime(dateTimeValue);
+    }
+}
